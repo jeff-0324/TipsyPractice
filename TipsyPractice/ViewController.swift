f991a98c9b3d948986df80fc5c69277d5596a002
jeff-0324 <jeffap324@gmail.com>
2023-09-06 23:44:54 +0900

    Initial Commit

---
+++ TipsyPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipsyPractice
+//
+//  Created by jae hoon lee on 2023/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
